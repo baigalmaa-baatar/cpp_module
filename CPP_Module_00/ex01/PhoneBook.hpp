d4e75bc71265ee7a13f142a7e81087537cc49b02
Baigalmaa BAATAR <bbaatar@42paris.fr>
2022-05-19 15:59:21 +0200

    adding yt tutorial

---
+++ CPP_Module_00/ex01/PhoneBook.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int age = 35;
+
+    cout << "There was once a man named John" << endl;
+    cout << "He was" << age << " years old" << endl;
+    cout << "He liked the name John" << endl;
+    cout << "But did not like being " << age << endl;
+    return 0;
+}
---
+++ yt_tutorial/example.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int age = 35;
+    string name = "John";
+
+    cout << "There was once a man named "<< name << endl;
+    cout << "He was " << age << " years old" << endl;
+    name = "Mike";
+    cout << "He liked the name "<< name << endl;
+    cout << "But did not like being " << age << endl;
+    return 0;
+}
